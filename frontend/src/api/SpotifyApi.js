54996e1018a8411fae05a8c1e61db6918733dcca
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2021-01-15 15:57:26 -0600

    Update spotify api util module

--- frontend/src/api/SpotifyApi.js
+++ frontend/src/api/SpotifyApi.js
@@ -1,7 +1,10 @@
 import axios from "axios";
-import Cookies from "js-cookie";
 
-export default axios.create({
-  baseURL: "https://api.spotify.com/v1",
-  headers: { Authorization: "Bearer " + Cookies.get("spotify-access-token") },
-});
+const spotify = (token = "") => {
+  return axios.create({
+    baseURL: "https://api.spotify.com/v1",
+    headers: { Authorization: "Bearer " + token },
+  });
+};
+
+export default spotify;
