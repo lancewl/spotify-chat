c178fa80a82cb027c66026831d0e24b1f8accd63
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2021-01-27 16:32:36 -0600

    Redesign the Login page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 import SpotifyTest from "./SpotifyTest";
@@ -9,16 +8,14 @@ import SpotifyTest from "./SpotifyTest";
 const App = () => {
   return (
     <Container component="main" maxWidth="xs">
-      <Box my={5} textAlign="center">
-        {Cookies.get("spotify-access-token") ? (
-          <div>
-            <LogoutButton />
-            <SpotifyTest />
-          </div>
-        ) : (
-          <LoginButton />
-        )}
-      </Box>
+      {Cookies.get("spotify-access-token") ? (
+        <div>
+          <LogoutButton />
+          <SpotifyTest />
+        </div>
+      ) : (
+        <LoginButton />
+      )}
     </Container>
   );
 };
--- frontend/src/components/LoginButton.js
+++ frontend/src/components/LoginButton.js
@@ -1,13 +1,45 @@
 import React, { useEffect } from "react";
+import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import PersonIcon from "@material-ui/icons/Person";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color: "#FFFFFF",
+    marginBottom: "5px",
+  },
+  button: {
+    padding: "8px 24px",
+    borderRadius: "24px",
+  },
+}));
 
 const LoginButton = () => {
   const loginUrl = "http://localhost:8888/login";
+  const classes = useStyles();
 
   return (
-    <Button variant="contained" href={loginUrl} color="primary">
-      Continue with Spotify
-    </Button>
+    <Box my={10} textAlign="center">
+      <Box mb={24}>
+        <Typography className={classes.root} variant="h1" gutterBottom>
+          Welcome!
+        </Typography>
+        <Typography className={classes.root} variant="body1" gutterBottom>
+          Spotify Chat is a chatroom sync with Spotify Playlist.
+        </Typography>
+      </Box>
+      <Button
+        className={classes.button}
+        variant="contained"
+        href={loginUrl}
+        color="primary"
+        startIcon={<PersonIcon />}
+      >
+        Continue with Spotify
+      </Button>
+    </Box>
   );
 };
 
